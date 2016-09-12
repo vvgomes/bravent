907235d9b21f485581ed56d8144ef3585205f848
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2016-09-12 08:32:07 -0400

    simplifies apply test cases

--- test/apply.test.js
+++ test/apply.test.js
@@ -1,40 +1,28 @@
 import defineApply from "../lib/apply";
 import assert from "assert";
 import { stub } from "sinon";
-import { evolve, inc } from "ramda";
 
 describe("apply()", () => {
-  const counterIncremented = stub().returns({ counter: 1 });
-  const apply = defineApply({ counterIncremented });
+  const apply = defineApply({
+    counterIncremented: stub().returns(1)
+  });
  
   it("calls appropriate handler for event and state", () => {
-    const state = { counter: 0 };
+    const state = 0;
     const event = { type: "counterIncremented" };
- 
-    assert.deepEqual(
-      apply(state, event),
-      { counter: 1 }
-    );
+    assert.equal(apply(state, event), 1);
   });
  
   it("returns unchanged state when no handler is found", () => {
-    const state = { counter: 0 };
+    const state = 0;
     const event = { type: "unknown" };
- 
-    assert.deepEqual(
-      apply(state, event),
-      { counter: 0 }
-    );
+    assert.equal(apply(state, event), 0);
   });
  
   it("returns unchanged state when event has no type", () => {
-    const state = { counter: 0 };
+    const state = 0;
     const event = {};
- 
-    assert.deepEqual(
-      apply(state, event),
-      { counter: 0 }
-    );
+    assert.equal(apply(state, event), 0);
   });
 });
 
