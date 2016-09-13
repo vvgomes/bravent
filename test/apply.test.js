ccd87a85ba225a8dbda14dd12e0daa59ec015667
Guilherme J. Tramontina <guilherme.tramontina@gmail.com>
2016-09-14 00:29:37 +1000

    style(lint): conform to editorconfig's settings

--- test/apply.test.js
+++ test/apply.test.js
@@ -6,23 +6,22 @@ describe("apply()", () => {
   const apply = defineApply({
     counterIncremented: stub().returns(1)
   });
- 
+
   it("calls appropriate handler for event and state", () => {
     const state = 0;
     const event = { type: "counterIncremented" };
     assert.equal(apply(state, event), 1);
   });
- 
+
   it("returns unchanged state when no handler is found", () => {
     const state = 0;
     const event = { type: "unknown" };
     assert.equal(apply(state, event), 0);
   });
- 
+
   it("returns unchanged state when event has no type", () => {
     const state = 0;
     const event = {};
     assert.equal(apply(state, event), 0);
   });
 });
-
